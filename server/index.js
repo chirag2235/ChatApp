e7a4d6b764360aba3c480c78bdfbb666db374715
Chirag <chiragpupneja2235@gmail.com>
2024-12-13 23:05:50 +0530

    u

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://chatapp-1-e74l.onrender.com/",
+    origin: "https://chatapp-1-e74l.onrender.com",
     credentials: true,
   },
 });
