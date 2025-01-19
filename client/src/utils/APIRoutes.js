8c5a6b72c086f9a5b040f837b09464f21637195d
Chirag <chiragpupneja2235@gmail.com>
2025-01-20 00:06:16 +0530

    updated code

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,6 +1,5 @@
-// export const host = "https://chatapp-pjgf.onrender.com";
+export const host = "https://chatapp-pjgf.onrender.com";
 // export const host ="http://localhost:5000"
-const host=process.env.host;
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
