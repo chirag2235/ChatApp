e2c09065feccb975f60474003fe9bbb150d8d176
Chirag <chiragpupneja2235@gmail.com>
2025-01-19 18:49:49 +0530

    dist issue resolve

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensures the build output goes to the 'dist' folder
+    emptyOutDir: true, // Clears the output folder before building
+  },
   define: {
-    'process.env': {},
-    'global': 'window'  // Polyfill global as window
-  }
-})
+    'process.env': {}, // Polyfill for `process.env` if needed
+    global: 'window', // Polyfill `global` as `window`
+  },
+});
